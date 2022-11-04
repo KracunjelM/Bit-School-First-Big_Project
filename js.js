b18dda42bd15c837b6df9a18c767b2b8212234e0
BIT Student <classroom@bgit.rs>
2022-11-04 12:40:23 +0100

    First-Big_Project

---
+++ js.js
@@ -0,0 +1,52 @@
+// konstruktivna funkcija
+
+// function BitStudent(name, lastname, dateOfBirth, placeOfBirth) {
+//     this.name = name;
+//     this.lastname = lastname;
+//     this.dateOfBirth = dateOfBirth;
+//     this.placeOfBirth = placeOfBirth;
+//     this.pet = {
+//         name: "Cica",
+//         rase: "Macka",
+//         color: "Black"
+//     }
+//     this.howOldIam = function() {
+//         var today = new Date();
+//         return today.getFullYear() - this.dateOfBirth;
+//     }
+//     this.sayFullName = function() {
+//        return this.name + this.lastname;
+//     }
+//     this.whereIamBorn = function() {
+//         return this.placeOfBirth;
+//     }
+//     this.iHavePet = function() {
+//         return this.pet;
+//     }
+//     this.kolikoImamGodZa100God = function() {
+//         return this.howOldIam() + 100;
+//     }
+//     this.lastnameChange = function(novoPrezime) {
+//         this.lastname = novoPrezime;
+//         // return this.lastname;
+//     }
+// }
+// var bojan = new BitStudent("Bojan", "Milenkovic", 1994, "Vrsac")
+// var milos = new BitStudent("Milos","Kracunjel",1997,"Vrsac")
+// console.log(bojan);
+// console.log(bojan.howOldIam())
+// console.log(bojan.sayFullName())
+// console.log(bojan.whereIamBorn())
+// console.log(bojan.iHavePet())
+// console.log(bojan.kolikoImamGodZa100God())
+// console.log(bojan.lastnameChange("Maric"))
+// console.log(bojan.lastname);
+// console.log(milos);
+// console.log(milos.howOldIam())
+// console.log(milos.sayFullName())
+// console.log(milos.whereIamBorn())
+// console.log(milos.iHavePet())
+// console.log(milos.kolikoImamGodZa100God())
+// console.log(milos.lastnameChange("Stankovic"))
+// console.log(milos.lastname);
+
